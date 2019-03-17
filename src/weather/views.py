74651d78ecbe3020d19c471ab903fcc312156116
Bhavesh Shukla <bhavesh.shuklatz@gmail.com>
2019-03-17 21:40:49 +0530

    Update views

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,34 +1,66 @@
 import requests
+
 from django.shortcuts import render
+from django.views import View
+
 from .models import City
 from .forms import CityForm
 
-def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=0a136f3cef869764f8d63741dbac7973'
 
-    if request.method == 'POST':
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        """
+            Get your own API Key by signing up on OpenMapWeather
+            URL - https://home.openweathermap.org/users/sign_up
+        """
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=YOUR-API-KEY'
+
+        form = CityForm()
+
+        cities = City.objects.all()
+        weather_data = []
+
+        for city in cities:
+            r = requests.get(url.format(city)).json()
+
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+            weather_data.append(city_weather)
+
+        context = {
+            'weather_data': weather_data,
+            'form': form,
+        }
+
+        return render(request, 'weather/weather.html', context)
+
+    def post(self, request, *args, **kwargs):
         form = CityForm(request.POST)
         form.save()
 
-    form = CityForm()
+        form = CityForm()
 
-    cities = City.objects.all()
-    weather_data = []
+        cities = City.objects.all()
+        weather_data = []
 
-    for city in cities:
-        r = requests.get(url.format(city)).json()
+        for city in cities:
+            r = requests.get(url.format(city)).json()
 
-        city_weather = {
-            'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon'],
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+            weather_data.append(city_weather)
+
+        context = {
+            'weather_data': weather_data,
+            'form': form,
         }
-        weather_data.append(city_weather)
 
-    context = {
-        'weather_data': weather_data,
-        'form': form,
-    }
-
-    return render(request, 'weather/weather.html', context)
+        return render(request, 'weather/weather.html', context)
