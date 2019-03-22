1a7ca6d30d5a51301a54ede87fdb4baf4ff101a7
bshukla3041 <bhavesh.shuklatz@gmail.com>
2019-03-22 16:29:00 +0530

    Fix form labels

--- src/weather/forms.py
+++ src/weather/forms.py
@@ -5,5 +5,6 @@ from .models import City
 class CityForm(forms.ModelForm):
     class Meta:
         model = City
+        labels = {'name': ''}
         fields = ['name']
-        widgets = {'name': forms.TextInput(attrs={'class': 'input', 'placeholder': 'City name'})}
+        widgets = {'name': forms.TextInput(attrs={'class': 'input form-css rounded-pill', 'placeholder': 'City name'})}
